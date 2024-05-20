86f5986737384ce23cbd9ccd66ffa8a9aaaf64e2
IzanamiiDevv <rafaeloli596@gmail.com>
2024-05-20 22:22:56 +0800

    s

--- Admin/index.php
+++ Admin/index.php
@@ -30,6 +30,7 @@ include '../Includes/session.php';
   <title>Dashboard</title>
   <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+  
   <link href="../css/ruang-admin.css" rel="stylesheet">
 </head>
 
