4ef6b9d3eb94f996d23e792cbd67dbef714ec270
IzanamiiDevv <rafaeloli596@gmail.com>
2024-05-20 23:20:55 +0800

    s

--- Admin/Includes/topbar.php
+++ Admin/Includes/topbar.php
@@ -38,7 +38,7 @@
             <li class="nav-item dropdown no-arrow">
               <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
                 aria-haspopup="true" aria-expanded="false">
-                <img class="img-profile rounded-circle" src="img/user-icn.png" style="max-width: 60px">
+                <img class="img-profile rounded-circle" src="../img/user-icn.png" style="max-width: 60px">
                 <span class="ml-2 d-none d-lg-inline text-white small"><b>Welcome <?php echo $fullName;?></b></span>
               </a>
               <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
