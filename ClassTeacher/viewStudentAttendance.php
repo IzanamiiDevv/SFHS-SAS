c367caec16e617df8db523fa2ab5db389437560b
IzanamiiDevv <rafaeloli596@gmail.com>
2024-05-21 00:26:36 +0800

    s

--- ClassTeacher/viewStudentAttendance.php
+++ ClassTeacher/viewStudentAttendance.php
@@ -21,7 +21,7 @@ include '../Includes/session.php';
   <title>Dashboard</title>
   <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-  <link href="css/ruang-admin.min.css" rel="stylesheet">
+  <link href="../css/ruang-admin.css" rel="stylesheet">
 
 <script>
     function typeDropDown(str) {
--- ClassTeacher/viewStudents.php
+++ ClassTeacher/viewStudents.php
@@ -29,7 +29,7 @@ $query = "SELECT tblclass.className,tblclassarms.classArmName
   <title>Dashboard</title>
   <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-  <link href="css/ruang-admin.min.css" rel="stylesheet">
+  <link href="../css/ruang-admin.css" rel="stylesheet">
 
 
 
