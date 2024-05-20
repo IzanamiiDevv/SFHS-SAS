fbd827e9a1e47d1ea565d9e1ab5dba1bec3ea1c9
IzanamiiDevv <rafaeloli596@gmail.com>
2024-05-21 00:30:22 +0800

    s

--- Admin/createClassTeacher.php
+++ Admin/createClassTeacher.php
@@ -142,7 +142,7 @@ if(isset($_POST['save'])){
 <?php include 'includes/title.php';?>
   <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-  <link href="css/ruang-admin.min.css" rel="stylesheet">
+  <link href="../css/ruang-admin.css" rel="stylesheet">
 
 
 
--- Admin/createUsers.php
+++ Admin/createUsers.php
@@ -34,7 +34,7 @@ include '../Includes/session.php';
   <title>COBIS - Dashboard</title>
   <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-  <link href="css/ruang-admin.min.css" rel="stylesheet">
+  <link href="../css/ruang-admin.css" rel="stylesheet">
 
 
 <script>
