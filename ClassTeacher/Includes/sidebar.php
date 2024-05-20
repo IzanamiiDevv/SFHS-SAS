4b2c18fd4662111cf408458585d4e672230f7ece
IzanamiiDevv <rafaeloli596@gmail.com>
2024-05-21 00:18:32 +0800

    d

--- ClassTeacher/Includes/sidebar.php
+++ ClassTeacher/Includes/sidebar.php
@@ -1,9 +1,9 @@
  <ul class="navbar-nav sidebar sidebar-light accordion" id="accordionSidebar">
       <a class="sidebar-brand d-flex align-items-center bg-gradient-primary justify-content-center" href="index.php">
         <div class="sidebar-brand-icon" >
-          <img src="img/logo/attnlgs.jpg">
+          <img src="../img/logo/attnlgs.jpg">
         </div>
-        <div class="sidebar-brand-text mx-3">AMS</div>
+        <div class="sidebar-brand-text mx-3">SFHS AMS</div>
       </a>
       <hr class="sidebar-divider my-0">
       <li class="nav-item active">
