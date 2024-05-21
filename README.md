867cf1d574e3a4667cda6a89acbf7d05b0d30cae
IzanamiiDevv <rafaeloli596@gmail.com>
2024-05-21 14:55:44 +0800

    s

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# SFHS-SAS
-San Francisco Student Attendace System
+![thumb37](https://github.com/abdulhaseeb456/blue-bg-responsive-navbar-26-february-2023/assets/99570180/4a43bf89-c8c4-496e-a501-1f2c8c3b1190)
--- login.php
+++ login.php
@@ -14,7 +14,7 @@ session_start();
     <meta name="description" content="">
     <meta name="author" content="">
     <link href="img/logo/attnlgs.jpg" rel="icon">
-    <title>SFHS - Login</title>
+    <title>SFHS | Login</title>
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
     <link href="css/ruang-admin.css" rel="stylesheet">
